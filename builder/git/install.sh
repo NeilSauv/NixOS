0886d076d656e5e2ee35dcda294916c5bd14875a
Neil <neil.sauvage2003@gmail.com>
2024-02-03 19:23:11 +0100

    fix: git

--- builder/git/install.sh
+++ builder/git/install.sh
@@ -30,4 +30,10 @@ EOF
 key_id=$(gpg --list-secret-keys --keyid-format LONG "$email" | grep sec | awk '{print $2}' | cut -d'/' -f2)
 
 sed -e "s/USER_MAJ/$maj/g" -e "s/USER_EMAIL/$email/g" -e "s/USER_KEY/$key_id/g" "default.nix" > "../../programs/git/default.nix"
+
+ssh-keygen -t ed25519 -C "$email"
+str=$(cat ~/.ssh/ed25519.pub)
+echo "Copy the folowing string into github : $str\n"
+echo "Presse ENTER when done"
+read -r nothing
 cd ..
