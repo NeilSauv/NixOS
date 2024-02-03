2e6a0bb933304434c5b951b0ca1d4f4db1fcc61e
Neil <neil.sauvage2003@gmail.com>
2024-02-03 19:15:27 +0100

    fix: vim

--- tools/vim/install.sh
+++ tools/vim/install.sh
@@ -3,19 +3,19 @@
 cd vim
 
 folders=(
-    "Vundle.vim:https://github.com/VundleVim/Vundle.vim"
-    "coc.nvim:https://github.com/neoclide/coc.nvim"
-    "delimitMate:https://github.com/Raimondi/delimitMate"
-    "DoxygenToolkit.vim:https://github.com/vim-scripts/DoxygenToolkit.vim"
-    "fzf.vim:https://github.com/junegunn/fzf"
-    "sensible:https://github.com/tpope/vim-sensible"
-    "vim-airline:https://github.com/vim-airline/vim-airline"
+    "Vundle.vim,https://github.com/VundleVim/Vundle.vim"
+    "coc.nvim,https://github.com/neoclide/coc.nvim"
+    "delimitMate,https://github.com/Raimondi/delimitMate"
+    "DoxygenToolkit.vim,https://github.com/vim-scripts/DoxygenToolkit.vim"
+    "fzf.vim,https://github.com/junegunn/fzf"
+    "sensible,https://github.com/tpope/vim-sensible"
+    "vim-airline,https://github.com/vim-airline/vim-airline"
 )
 
 extract_tuple() {
     local tuple="$1"
     local index="$2"
-    local separator=":"
+    local separator=","
     local element=$(echo "$tuple" | cut -d"$separator" -f"$((index + 1))")
     echo "$element"
 }
@@ -30,6 +30,10 @@ for tuple in "${folders[@]}"; do
         (git clone $path $name)
     fi
 done
+
+cd coc.nvim
+npm ci
+cd ..
 
 cd ../../
 cp .vimrc ~/
