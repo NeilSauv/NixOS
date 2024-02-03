8381b0b5bba2db5e6e99341d3a70ad2618a9c150
Neil <neil.sauvage2003@gmail.com>
2024-02-03 16:41:36 +0100

    multiple fix

--- users/install.sh
+++ users/install.sh
@@ -1,9 +1,21 @@
 cd users/
 res=$(ls -d */ 2> /dev/null 1> /dev/null; echo $?)
+present=true
 
 if [ $res != "0" ]
 then
     mkdir "$NAME_USER"
+    present=false
+fi
+
+maj="$NAME_USER"
+maj=$(echo "$maj" | sed 's/./\u&/')
+
+sed -e "s/USER_NAME/$NAME_USER/g" -e "s/USER_MAJ/$maj/g" "../builder/home.nix" > "$NAME_USER/home.nix"
+
+if [ $present = false ]
+then
+    home-manager switch --flake .
 fi
 
 cd ..
