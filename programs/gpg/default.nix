805d9e70be8c40db8933be3b3b751189d4f5ace4
Neil <neil.sauvage2003@gmail.com>
2024-02-04 21:14:44 +0100

    Add gpg-agent

--- programs/gpg/default.nix
+++ programs/gpg/default.nix
@@ -1,12 +1,17 @@
 { ... }:
 
 {
-  programs.gpg = {                                                      
+  programs.gpg = {
     enable = true;
   };
 
   services.gpg-agent = {
     enable = true;
     pinentryFlavor = "gtk2";
+    extraConfig = ''
+      default-cache-ttl 3600
+      max-cache-ttl 86400
+    '';
   };
 }
+
